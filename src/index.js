0eb40b34f05bbaa8465ed533837d488aa57ba4c2
Pranav Sai <pranavxd142@gmail.com>
2023-07-12 12:52:44 -0500

    St arted firebase auth implementation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, onAuthStateChanged } from "firebase/auth"
+import { getAuth, onAuthStateChanged, connectAuthEmulator, signInWithEmailAndPassword } from "firebase/auth"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+//Document Elements
+const btnLogin = document.getElementById("btnLogin")
+const txtEmail = document.getElementById("txtEmail")
+const txtPassword = document.getElementById("txtPassword")
+function getElement(id) {
+    return document.getElementById("btnLogin")
+}
+function getValue(element) {
+    if(element.value != null) {
+        return element.value
+    } else {
+        return ""
+    }
+}
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyAQ5Sty_qAzOBtd_h2gFTGEC5sHH3_fNWE",
@@ -18,7 +32,16 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app)
+connectAuthEmulator(auth, "http://localhost:9099")
 
+const loginEmailPassword = async () => {
+    const loginEmail = getValue(getElement("txtEmail"))
+    const loginPassword = getValue(getElement("txtEmail"))
+
+    const userCredentials = await signInWithEmailAndPassword(auth, loginEmail, loginPassword)
+    console.log(userCredentials.user)
+}
+getElement("btnLogin").addEventListener("click", loginEmailPassword)
 onAuthStateChanged(auth, user => {
     if(user !== null) {
         console.log("User logged in")
